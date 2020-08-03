28d65ae26ba7a011f19c2a7feb3fa16c397619e9
João Pedro Dante <joaopdante@gmail.com>
2020-08-03 04:03:28 -0300

    💚 🐛 Atualiza e corrige script RGD

--- .rgd/run.lua
+++ .rgd/run.lua
@@ -1,28 +1,27 @@
--- Version 1.1
-if branch == "development" then
-    status = run("npm", "install")
+-- Version 1.3.2
+
+function executeWithStatus(filename, args)
+    status = run(filename, args)
     setStatus(status)
-    status = run("npm", "run build --node-flags --max-old-space-size=512")
-    setStatus(status)
-    status = run("rm", "-rfv /home/rgd/deploy/dev/frontend/")
-    setStatus(status)
-    status = run("cp", "-rv ./build/ /home/rgd/deploy/dev/frontend/")
-    setStatus(status)
-    status = run("rm", "-rfv /home/rgd/deploy/dev/landing/")
-    setStatus(status)
-    status = run("cp", "-rv ./landing/ /home/rgd/deploy/dev/landing/")
-    setStatus(status)
-elseif branch == "master" then
-    status = run("npm", "install")
-    setStatus(status)
-    status = run("npm", "run build --node-flags --max-old-space-size=512")
-    setStatus(status)
-    status = run("rm", "-rfv /home/rgd/deploy/dist/frontend/")
-    setStatus(status)
-    status = run("cp", "-rv ./build/ /home/rgd/deploy/dist/frontend/")
-    setStatus(status)
-    status = run("rm", "-rfv /home/rgd/deploy/dist/landing/")
-    setStatus(status)
-    status = run("cp", "-rv ./landing/ /home/rgd/deploy/dist/landing/")
-    setStatus(status)
+    return status != 0
 end
+
+function runScript()
+    if branch == "development" then
+        if executeWithStatus("npm", "install") then return end
+        if executeWithStatus("npm", "run build --node-flags --max-old-space-size=512") then return end
+        if executeWithStatus("rm", "-rfv /home/rgd/deploy/dev/frontend/") then return end
+        if executeWithStatus("cp", "-rv ./build/ /home/rgd/deploy/dev/frontend/") then return end
+        if executeWithStatus("rm", "-rfv /home/rgd/deploy/dev/landing/") then return end
+        if executeWithStatus("cp", "-rv ./landing/ /home/rgd/deploy/dev/landing/") then return end
+    elseif branch == "master" then
+        if executeWithStatus("npm", "install") then return end
+        if executeWithStatus("npm", "run build --node-flags --max-old-space-size=512") then return end
+        if executeWithStatus("rm", "-rfv /home/rgd/deploy/dist/frontend/") then return end
+        if executeWithStatus("cp", "-rv ./build/ /home/rgd/deploy/dist/frontend/") then return end
+        if executeWithStatus("rm", "-rfv /home/rgd/deploy/dist/landing/") then return end
+        if executeWithStatus("cp", "-rv ./landing/ /home/rgd/deploy/dist/landing/") then return end
+    end
+end
+
+runScript()
